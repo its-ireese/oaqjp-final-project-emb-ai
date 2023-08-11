008d8874fa6b76920c66ad7f510d219ec4086d24
Derrice <derricewright@gmail.com>
2023-08-11 02:39:10 -0400

    Full Project v2

--- server.py
+++ server.py
@@ -1,18 +1,16 @@
-''' Executing this function initiates the application of sentiment
-    analysis to be executed over the Flask channel and deployed on
-    localhost:5000.
+''' Executing the application of Emotion Detection over the Flask channel 
+    and deployed on localhost:5002.
 '''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
-
 @app.route("/emotionDetector")
 def sent_detector():
-    ''' This code receives text from the HTML interface and runs emotion detection 
-    over it using emotion_dection() function. The output returned shows the 
-    different emotion scores for the text provided
+    ''' This code receives text and runs emotion detection function
+    over it using emotion_dection(). The output shows the different emotion 
+    scores for the text provided.
     '''
     text_to_analyze = request.args.get('textToAnalyze')
     dominant_emotion = emotion_detector(text_to_analyze)
@@ -24,11 +22,8 @@ def sent_detector():
 
 @app.route("/")
 def render_index_page():
-    ''' This code receives text from the HTML interface and runs emotion detection
-    over it using emotion_dection() function. The output returned shows the 
-    different emotion scores for the text provided '''
+    ''' Rendering of the main application page over the Flask channel '''
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
-    
